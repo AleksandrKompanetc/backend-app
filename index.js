7a50d71e02057b998fdc55e34a52f2ff02cb9084
AleksandrKompanetc <moneymacker7@gmail.com>
2025-11-12 23:59:37 +0200

    changed server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3003;
 
 app.get('/', (req, res) => {
-  res.send('Hello world!!!!!Now');
+  const time = new Date().toLocaleTimeString();
+  res.send(`Сервер обновлён в: ${time}`);
+  // res.send('I want to become a backend developer!');
+  // console.log('✅ Сервер запущен!');
 })
 
 app.listen(port, () => {
