ce4f39f787509a5b061a2baa2a391475250b2d76
AleksandrKompanetc <moneymacker7@gmail.com>
2025-11-13 23:51:41 +0200

    added src folder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3008;
+const port = 3009;
 
 app.get('/', (req, res) => {
-  res.send('Hello');
+  res.send('Hello Yo');
 })
 
 app.listen(port, () => {
