4d437f35393324a8d5d95a83ff5a84d57da1b483
AleksandrKompanetc <moneymacker7@gmail.com>
2025-11-15 01:01:07 +0200

    installed nodemon

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3009;
 
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
