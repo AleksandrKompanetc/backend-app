710fa0500b2ee6f82828975ca983c7af9e4c821d
AleksandrKompanetc <moneymacker7@gmail.com>
2025-11-20 23:46:55 +0200

    added get request

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3011;
 
+
+
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello world! Yo!');
+})
+
+app.get('/products', (req: Request, res: Response) => {
+  res.send(products);
 })
 
 app.listen(port, () => {
