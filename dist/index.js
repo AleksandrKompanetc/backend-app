df2957111ce60261b7697e6678a19b1832b5544b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-11-22 15:29:25 +0200

    added if else for get request

--- dist/index.js
+++ dist/index.js
@@ -45,8 +45,13 @@ app.get('/products', (req, res) => {
     res.send(products);
 });
 app.get('/products/:productTitle', (req, res) => {
-    let tomato = products.find(p => p.title === req.params.productTitle);
-    res.send(tomato);
+    let product = products.find(p => p.title === req.params.productTitle);
+    if (product) {
+        res.send(product);
+    }
+    else {
+        res.send(404);
+    }
 });
 app.get('/addresses', (req, res) => {
     res.send(addresses);
--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,13 @@ app.get('/products', (req: Request, res: Response) => {
 })
 
 app.get('/products/:productTitle', (req: Request, res: Response) => {
-  let tomato = products.find(p => p.title === req.params.productTitle);
-  res.send(tomato);
+  let product = products.find(p => p.title === req.params.productTitle);
+  if (product) {
+    res.send(product);
+  } else {
+    res.send(404);
+  }
+  
 })
 
 app.get('/addresses', (req: Request, res: Response) => {
