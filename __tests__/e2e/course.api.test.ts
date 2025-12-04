0e967ecf8618bc20e7a646e0730f067699035409
AleksandrKompanetc <moneymacker7@gmail.com>
2025-12-04 20:51:43 +0200

    added new jest test

--- __tests__/e2e/course.api.test.ts
+++ __tests__/e2e/course.api.test.ts
@@ -4,6 +4,12 @@ describe('/course', () => {
   it('should return 200 and empty array', async () => {
     await request(app)
       .get('/courses')
-      .expect(200, []);
-  });
+      .expect(200, [])
+  })
+
+  it('should return 404 for not existing course', async () => {
+    await request(app)
+      .get('/courses/9999')
+      .expect(404)
+  })
 })
