902c92ecae804e2149d25baa0990e437ce0874f6
AleksandrKompanetc <moneymacker7@gmail.com>
2025-12-18 22:38:38 +0200

    created app

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+export const app = express()
+
+export const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+}
+
