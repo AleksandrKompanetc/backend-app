f7d72d5fc4f9cf0c9b9b665ec0654ac85b07662a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-12-27 23:43:01 +0200

    listening port

--- src/index.js
+++ src/index.js
@@ -5,3 +5,7 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('Hello Node JS + Express');
 })
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+})
