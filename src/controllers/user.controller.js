e401c001f03b70669eb9340f67c50ebe8a9c2f95
AleksandrKompanetc <moneymacker7@gmail.com>
2026-01-04 23:56:21 +0200

    added controllers

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,31 @@
+let users = [];
+
+exports.getUsers = (req, res) => {
+  res.json(users);
+}
+
+exports.getUserById = (req, res) => {
+
+  const user = users.find(u => u.id === Number(req.params.id));
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  res.json(user);
+}
+
+exports.createUser = (req, res) => {
+  const user = { 
+    id: Date.now(), 
+    name: req.body.name 
+  };
+
+  users.push(user);
+  res.status(201).json(user);
+}
+
+exports.deleteUser = (req, res) => {
+  users = users.filter(u => u.id !== Number(req.params.id));
+  res.send('User deleted');
+}
