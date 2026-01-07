07c0310d32da303715ec2d38bd83f152552f4412
AleksandrKompanetc <moneymacker7@gmail.com>
2026-01-08 01:04:54 +0200

    using asyncHandler

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const errorMiddleware = require('./middleware/error.middleware');
 const userRoutes = require('./routes/user.routes');
 const logger = require('./middleware/logger');
 
+const asyncHandler = require('./utils/asyncHadler');
+exports.getUsers = asyncHandler(async (req, res) => {
+  res.json(users)
+})
+
 const app = express();
 
 app.use(express.json());
