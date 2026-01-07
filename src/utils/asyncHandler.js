540044d5ceb33f775ea23aa91c78e176e6d4e9f6
AleksandrKompanetc <moneymacker7@gmail.com>
2026-01-08 01:07:40 +0200

    added services

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,8 @@
 module.exports = fn => (req, res, next) => {
   Promise.resolve(fn(req, res, next)).catch(next);
 }
+
+const asyncHandler = require('./utils/asyncHadler');
+exports.getUsers = asyncHandler(async (req, res) => {
+  res.json(users)
+})
